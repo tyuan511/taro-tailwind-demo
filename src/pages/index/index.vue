79ed6807f1c38c8cb6aabe92620a9f8eebd123a1
tyuan <yuantang@email.cn>
2022-08-15 17:58:48 +0800

    update

---
+++ src/pages/index/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <view class="text-[48px] text-[#ff0000]">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import './index.css';
+
+const msg = ref('Hello world');
+</script>
